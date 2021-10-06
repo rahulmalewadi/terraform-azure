a4512345c9f16be48bfabdc398e5a06b06dd91e5
beluga_777 <82512132+rahulmalewadi@users.noreply.github.com>
2021-10-06 15:05:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,86 +1,29 @@
-#############################################################################
-# TERRAFORM CONFIG
-#############################################################################
+
 
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.0"
+      version = "=2.46.0"
     }
   }
 }
 
-#############################################################################
-# VARIABLES
-#############################################################################
-
-variable "resource_group_name" {
-  type = string
-  default = "timepass"
-}
-
-variable "location" {
-  type    = string
-  default = "eastus"
-}
-
-
-variable "vnet_cidr_range" {
-  type    = string
-  default = "10.0.0.0/16"
-}
-
-variable "subnet_prefixes" {
-  type    = list(string)
-  default = ["10.0.0.0/24", "10.0.1.0/24"]
-}
-
-variable "subnet_names" {
-  type    = list(string)
-  default = ["web", "database"]
-}
-
-#############################################################################
-# PROVIDERS
-#############################################################################
-
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
 
-#############################################################################
-# RESOURCES
-#############################################################################
-
-resource "azurerm_resource_group" "vnet_main" {
-  name     = var.resource_group_name
-  location = var.location
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
 
-module "vnet-main" {
-  source              = "Azure/vnet/azurerm"
-  version             = "~> 2.0"
-  resource_group_name = azurerm_resource_group.vnet_main.name
-  vnet_name           = var.resource_group_name
-  address_space       = [var.vnet_cidr_range]
-  subnet_prefixes     = var.subnet_prefixes
-  subnet_names        = var.subnet_names
-  nsg_ids             = {}
-
-  tags = {
-    environment = "dev"
-    costcenter  = "it"
-
-  }
-
-  depends_on = [azurerm_resource_group.vnet_main]
-}
-
-#############################################################################
-# OUTPUTS
-#############################################################################
-
-output "vnet_id" {
-  value = module.vnet-main.vnet_id
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
 }
